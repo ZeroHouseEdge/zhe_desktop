e5801beb2bf9ae9e6c29ffc09dd6f22e0591e034
Jack Mallers <jimmymowschess@gmail.com>
2016-06-16 12:37:30 -0500

    refactored signup code to signup container

--- app/containers/SignupContainer.js
+++ app/containers/SignupContainer.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
-import Signup from '../components/Signup';
+import { browserHistory } from 'react-router'
+import { Link } from 'react-router';
+import Logo from '../components/Logo';
+import styles from '../components/Signup.css';
+import Default from '../components/Signup/Default';
+import Create from '../components/Signup/Create';
+import Load from '../components/Signup/Load';
 
 export default class SignupContainer extends Component {
-  render() {
-    return (
-      <Signup />
-    );
-  }
+  constructor(props, context) {
+      super(props, context);
+      this.createWallet = this.createWallet.bind(this);
+      this.loadWallet = this.loadWallet.bind(this);
+      this.backToDefault = this.backToDefault.bind(this);
+
+      this.state = { show: false };
+   }
+
+   backToDefault() {
+      this.setState({ show: false });
+   }
+
+   createWallet() {
+      this.setState({ show: "create" });
+   }
+
+   loadWallet() {
+      this.setState({ show: "load" });
+   }
+
+   render() {
+      return (
+         <div className={styles.container}>
+            <Logo />
+            <div>
+               {(() => {
+                  switch (this.state.show) {
+                     case "create": return <Create back={this.backToDefault} />;
+                     case "load": return <Load back={this.backToDefault} />;
+                     default: return <Default create={this.createWallet} load={this.loadWallet} />;
+                  }
+               })()}
+            </div>
+         </div>
+      );
+   }
 }
